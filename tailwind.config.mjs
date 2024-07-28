45d7f251cb0baf80efd022814504cded013a915a
Royer G. <royjuni3431@gmail.com>
2024-07-28 17:32:01 -0500

    feat: add new pages and start the development

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
